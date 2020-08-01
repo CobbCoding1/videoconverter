7b3aa71f25ab4977027984518a40b699aab869f6
CobbCoding1 <49418487+CobbCoding1@users.noreply.github.com>
2020-07-31 20:46:41 -0400

    Add files via upload

--- video.py
+++ video.py
@@ -1,7 +1,7 @@
 import os
 from moviepy.editor import *
 import tkinter as tk
-from tkinter import Tk, filedialog, Button
+from tkinter import Tk, filedialog, Button, Entry, StringVar
 
 
 def file():
@@ -11,18 +11,22 @@ def file():
 
 
 def convert(file):
+    nameOut = nameVar.get()
+
     video = VideoFileClip(os.path.join(file))
     dir1 = os.getcwd()
-    video.audio.write_audiofile(os.path.join(dir1, 'output.mp3'))
+    video.audio.write_audiofile(os.path.join(dir1 + '/' + nameOut + '.mp3'))
 
 
 root = Tk()
 
+nameVar = StringVar(root)
+
 btn = Button(text='Convert', command=file, height=5, width=15)
+name = Entry(width=15, textvariable=nameVar)
 
 btn.place(x=50, y=50)
-
-filename = 'sweetchildofmeme.mp4'
+name.place(x=60, y=20, height=25)
 
 
 root.mainloop()
